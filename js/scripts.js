4a76ac3e5545283a9051c5a35e09c5cf7fabca6a
Philipp Landmann <landmann.philipp@gmx.de>
2020-12-17 17:42:00 +0100

    Created animations and a jump to top button

--- js/scripts.js
+++ js/scripts.js
@@ -113,3 +113,23 @@ function searchSubmit() {
 function copyToClipboard(iconId) {
 	window.prompt('To copy, press CTRL+C: ', iconId)
 }
+
+window.onscroll = function () {
+	scrollFunction()
+}
+function scrollFunction() {
+	if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	) {
+		topBtn.style.display = 'block'
+	} else {
+		topBtn.style.display = 'none'
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0
+	document.documentElement.scrollTop = 0
+}
